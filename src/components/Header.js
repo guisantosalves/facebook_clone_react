d33760982214247376c57ef81cb23c75b2e384fe
guisantosalves <workguisantos@gmail.com>
2022-06-03 12:47:06 -0400

    finishing header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,14 @@ import HomeIcon from "@mui/icons-material/Home";
 import FlagIcon from "@mui/icons-material/Flag";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
 import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
-import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 function Header(props) {
   return (
@@ -23,41 +30,56 @@ function Header(props) {
         </div>
       </div>
 
-      <div className="header__middle">
+      <div className="header__center">
 
         {/* icons for options */}
         <div className="header__option">
-          <HomeIcon 
-            fontSize="large" 
-          />
+          <HomeIcon fontSize="large" />
         </div>
 
         <div className="header__option">
-          <FlagIcon 
-            fontSize="large" 
-          />
+          <FlagIcon fontSize="large" />
         </div>
 
         <div className="header__option">
-          <SubscriptionsOutlinedIcon 
-            fontSize="large" 
-          />
+          <SubscriptionsOutlinedIcon fontSize="large" />
         </div>
 
-        <div>
-          <StorefrontOutlinedIcon 
-            fontSize="large" 
-          />
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
         </div>
 
-        <div>
-            <SupervisedUserCircleIcon 
-                fontSize="large"
-            />
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
         </div>
+
       </div>
 
-      <div className="header__right"></div>
+      
+      <div className="header__right">
+        {/* avatar and some icons */}
+        <div className="header__info">
+
+            <AccountCircleIcon fontSize="large"/>
+            <h4>Guilherme Santos</h4>
+
+            {/* doing a icon button */}
+            <IconButton>
+                <Add />
+            </IconButton>
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsActiveIcon />
+            </IconButton>
+            <IconButton>
+                <ExpandMoreIcon />
+            </IconButton>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
