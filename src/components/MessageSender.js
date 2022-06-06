89eeba14722e02ab13fc03ce510002b204aae277
guisantosalves <workguisantos@gmail.com>
2022-06-06 12:39:16 -0400

    making the postbar

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -1,16 +1,52 @@
 import { Avatar } from "@mui/material";
 import React from "react";
 import './MessageSender.css';
+import { Videocam } from "@mui/icons-material";
+import { PhotoLibrary } from "@mui/icons-material";
+import { InsertEmoticon } from "@mui/icons-material";
 
 function MessageSender(){
+
+    const handleSubmit = (e) => {
+        //avoiding the reload of the page
+        e.preventDefault();
+
+
+    }
+
     return(
         <div className="messageSender">
             <div className="messageSender__top">
                 <Avatar />
+                <form>
+                    <input
+                        className="messageSender__input"
+                        placeholder="What's on your mind?"
+                    />
+                    
+                    <input placeholder="image URL (Optional)"/>
+
+                    <button onClick={handleSubmit} type="submit">
+                        Hidden submit
+                    </button>
+                </form>
             </div>
             
             <div className="messageSender__bottom">
+             <div className="messageSender__option">
+                <Videocam style={{color: "red"}}/>
+                <h3>Live Video</h3>
+             </div>
 
+             <div className="messageSender__option">
+                <PhotoLibrary style={{color: "green"}}/>
+                <h3>Photo/Video</h3>
+             </div>
+
+             <div className="messageSender__option">
+                <InsertEmoticon style={{color: "orange"}}/>
+                <h3>Feeling/Activity</h3>
+             </div>
             </div>
         </div>
     )
