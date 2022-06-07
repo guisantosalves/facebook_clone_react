c2ccb72cfa968c38e1609f3fb7ba4b0e01b46402
guisantosalves <workguisantos@gmail.com>
2022-06-07 12:28:28 -0400

    friend list done

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -32,22 +32,32 @@ function Widgets() {
             <div className="frind_list">
                 <div className="row_friend">
                     <Avatar src="https://i.pinimg.com/564x/96/82/c7/9682c7463bffeddd26c4b2cf931d952e.jpg"/>
-                    <p>Eduarda yasaka</p>
+                    <p>Joao vitor lopes</p>
                 </div>
 
                 <div className="row_friend">
-                    <Avatar src="https://i.pinimg.com/564x/96/82/c7/9682c7463bffeddd26c4b2cf931d952e.jpg"/>
-                    <p>Eduarda yasaka</p>
+                    <Avatar src="https://i.pinimg.com/564x/fe/0d/a3/fe0da3b2278cfc0a62c06ac2a2e7c6ea.jpg"/>
+                    <p>Henrique alves</p>
                 </div>
 
                 <div className="row_friend">
-                    <Avatar src="https://i.pinimg.com/564x/96/82/c7/9682c7463bffeddd26c4b2cf931d952e.jpg"/>
-                    <p>Eduarda yasaka</p>
+                    <Avatar src="https://i.pinimg.com/564x/76/a9/c4/76a9c40326825346749fb03a7506803a.jpg"/>
+                    <p>Lucas viking</p>
                 </div>
 
                 <div className="row_friend">
-                    <Avatar src="https://i.pinimg.com/564x/96/82/c7/9682c7463bffeddd26c4b2cf931d952e.jpg"/>
-                    <p>Eduarda yasaka</p>
+                    <Avatar src="https://i.pinimg.com/236x/88/5e/8d/885e8dcda0d07df8fa9b9b5f0446118a.jpg"/>
+                    <p>Gabriel biudes</p>
+                </div>
+
+                <div className="row_friend">
+                    <Avatar src="https://i.pinimg.com/564x/7b/fb/ba/7bfbbaa39ff2e0dc465ccb6e23560c96.jpg"/>
+                    <p>Bruno Gomes</p>
+                </div>
+
+                <div className="row_friend">
+                    <Avatar src="https://i.pinimg.com/564x/32/18/c9/3218c92ded0ebace8662dd756d460b2b.jpg"/>
+                    <p>Beatriz da silva</p>
                 </div>
             </div>
         </div>
